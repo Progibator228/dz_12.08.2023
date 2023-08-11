0d9c6b0ee2733c0c50c3a9548c9e8632c08dc864
«Mouse» <Taxon2494@mail.ru>
2023-08-12 01:11:16 +0500

    Фикс текста

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа 
 // и выдаёт, какое число большее, а какое меньшее.
+
 Console.WriteLine("Введите первое целое число");
 int num1 = Convert.ToInt32 ( Console.ReadLine () );
 Console.WriteLine("Введите второе целое число");
@@ -7,12 +8,12 @@ int num2 = Convert.ToInt32 ( Console.ReadLine () );
 
 if(num1 > num2 )
 {
-    Console.WriteLine(num1);
+    
     Console.Write($"Максимальное число {num1}");
 }
 if(num2 > num1 )
 {
-    Console.WriteLine(num2);
+    
     Console.Write($"Максимальное число {num2}");
 }
 if(num2 == num1 )
